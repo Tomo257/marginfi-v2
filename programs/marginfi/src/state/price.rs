2b9d76c6e6fb178a17814de7ea06cbc74b3ba419
root <root@33819.example.com>
2025-08-21 18:18:26 +0200

    Fix discriminator comparison for switchboard-on-demand 0.5.1

--- programs/marginfi/src/state/price.rs
+++ programs/marginfi/src/state/price.rs
@@ -579,7 +579,7 @@ pub fn parse_swb_ignore_alignment(data: Ref<&mut [u8]>) -> MarginfiResult<PullFe
         return err!(MarginfiError::SwitchboardInvalidAccount);
     }
 
-    if data[..8] != PullFeedAccountData::DISCRIMINATOR {
+    if data[..8] != *PullFeedAccountData::DISCRIMINATOR {
         return err!(MarginfiError::SwitchboardInvalidAccount);
     }
 
